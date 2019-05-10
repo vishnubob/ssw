0cb4c44c74a0664739bbc4ba520663a123e7ca49
Giles Hall <giles@polymerase.org>
2019-05-10 12:37:37 -0400

    fix travis file for python3.7 support

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ config = {
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
